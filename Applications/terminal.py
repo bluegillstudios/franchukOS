fecadc46aa4903277cca56acc103d65f422bfe00
Owen Gaydosz <owgydz@proton.me>
2025-05-28 19:14:24 -0400

    Increment VERSION to 31.0 (31.0.2279.101)

--- Applications/terminal.py
+++ Applications/terminal.py
@@ -68,7 +68,7 @@ class Terminal(tk.Toplevel):
             "arch": self.show_architecture,
             "python": self.run_python_interpreter,
             "help": self.show_help,
-            "version": lambda args: "FranchukOS version 30.1.1.7977.191 (codenamed Rainier). Terminal version v0.7.4.2",
+            "version": lambda args: "FranchukOS version 31.0.2279.101 (codenamed Madre). Terminal version v0.7.4.2",
             "rename": self.rename_file,
         }
 
